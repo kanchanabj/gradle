99b5e1df679003482fb7d6905fac7aa2f8c2e5aa
Adam Brown <adamb@gradle.com>
2018-02-08 15:57:54 -0700

    Updated asm to 6.1-beta2 and switch cglib:cglib-nodep to cglib:cglib

--- subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/junit/BuildSrcSpockIntegrationTest.groovy
+++ subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/junit/BuildSrcSpockIntegrationTest.groovy
@@ -33,7 +33,7 @@ dependencies {
 
     testCompile 'junit:junit:4.12@jar',
         'org.spockframework:spock-core:1.0-groovy-2.4@jar',
-        'cglib:cglib-nodep:2.2',
+        'cglib:cglib:3.2.6',
         'org.objenesis:objenesis:1.2'
 }
 """
